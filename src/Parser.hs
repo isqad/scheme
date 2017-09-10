a591bc293634f11165e247663cd5d80f17c7223e
Andrew N. Shalaev <isqad88@yandex.ru>
2017-09-10 13:14:13 +0500

    fix: introduce LispVal ADT

--- src/Parser.hs
+++ src/Parser.hs
@@ -3,6 +3,13 @@ module Main where
 import System.Environment
 import Text.ParserCombinators.Parsec hiding (spaces)
 
+data LispVal = Atom String
+             | List [LispVal]
+             | DottedList [LispVal] LispVal
+             | Number [Integer]
+             | String String
+             | Bool Bool
+
 main :: IO ()
 main = do args <- getArgs
           putStrLn (readExpr (args !! 0))
@@ -10,7 +17,11 @@ main = do args <- getArgs
 symbol :: Parser Char
 symbol = oneOf "!$%&|*+-/:<=?>@~_^#"
 
+spaces :: Parser ()
+spaces = skipMany1 space
+
 readExpr :: String -> String
-readExpr input = case parse symbol "list" input of
+readExpr input = case parse symbol "lisp" input of
   Left err -> "No match: " ++ show err
   Right val -> "Found value"
+
